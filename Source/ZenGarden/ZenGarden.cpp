4f2609db07b107ca54a31e9c2c200a29fdc9eb1d
Zhdanov Dmitry <d.zhdanov@dragon-machines.com>
2023-02-04 20:54:08 +0400

    Start

---
+++ Source/ZenGarden/ZenGarden.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ZenGarden.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ZenGarden, "ZenGarden" );
+
+DEFINE_LOG_CATEGORY(LogZenGarden)
+ 
